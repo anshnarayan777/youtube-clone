069903951f14cd2fdf73d60dc3b8295dfe91bf15
Mrityunjay <pandeyansh662@gmail.com>
2026-06-01 10:32:37 +0530

    Enhanced comment system and created video data structure

---
+++ frontend/src/data/videos.jsx
@@ -0,0 +1,32 @@
+const videos = [
+  {
+    id: 1,
+    title: "React Tutorial",
+    channel: "CodeWithHarry",
+    views: "10K",
+    thumbnail: "https://picsum.photos/400/250?random=1"
+  },
+  {
+    id: 2,
+    title: "Node.js Crash Course",
+    channel: "Traversy Media",
+    views: "25K",
+    thumbnail: "https://picsum.photos/400/250?random=2"
+  },
+  {
+    id: 3,
+    title: "MongoDB Guide",
+    channel: "Programming Guru",
+    views: "7K",
+    thumbnail: "https://picsum.photos/400/250?random=3"
+  },
+  {
+    id: 4,
+    title: "Tailwind CSS",
+    channel: "Frontend Master",
+    views: "15K",
+    thumbnail: "https://picsum.photos/400/250?random=4"
+  }
+];
+
+export default videos;
